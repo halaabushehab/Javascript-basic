6261db7f76c25d9aeb6b834c4320eb7813a9d31c
halaabushehab <hala.abushiihab@gmail.com>
2024-12-26 23:00:28 +0300

    javascript

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Javascript-basic
+
+
+### JavaScript is a versatile, high-level programming language widely used for adding interactivity, dynamic content, and functionality to websites and web applications. It enables tasks like manipulating HTML/CSS, handling events, and making asynchronous requests to servers. Its flexibility, along with modern frameworks and libraries, makes it essential for front-end and back-end development.
+
+#### I completed the task of evaluating each of the following JavaScript expressions and showing their values.
---
+++ main.js
@@ -0,0 +1,60 @@
+
+//Q1:
+
+console.log(-5*3)  //-15
+console.log("JavaScript" +50)//JavaScript50
+console.log(17 % 5)//2
+
+
+
+console.log(5 % 17)//5
+console.log(5/10)//0.5
+console.log((4 === '4'))//false
+
+
+
+console.log((4 != 5))//true
+console.log(7 <= 8)//true
+console.log("Hello"+5)//Hello5
+
+
+let  x=4.5;
+console.log(Math.ceil(x) - Math.floor(x))//1
+console.log(Math.pow(x,2))//20.25
+
+//Q2:
+
+ let userInput = prompt("Please enter a number");
+
+ let number = Number(userInput);
+
+ alert("the number you entered:  " + number);
+
+//Q3:
+let number_1 = parseFloat(prompt("Enter first number:"));
+let number_2 = parseFloat(prompt("Enter second number:"));
+let sum = number_1 + number_2;
+alert(" sum : " + sum);
+
+
+//Q4:
+ let number_3 = parseFloat(prompt("Enter first number:"));
+let number_4 = parseFloat(prompt("Enter second number:"));
+let product = number_3 * number_4;
+alert(" product : " + product);
+
+
+
+
+//Q5:
+let dividing = number_3 / number_4;
+alert(" the result of dividing " +  dividing);
+
+
+
+//Q6:
+ let h=12;
+let a= 5;
+ let sum_2 = a +h;
+ alert("The sum of " + h + " and " + a + " is: " + sum_2);
+ console.log(sum_2)
